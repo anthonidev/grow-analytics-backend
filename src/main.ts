47d1781281fa4f06a90f2c9c39583995cb7e2bc1
anthonidev <softwaretoni21@gmail.com>
2024-11-12 18:53:49 -0500

    fix gateway

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config)
   SwaggerModule.setup('docs', app, document)
 
-  await app.listen(process.env.PORT || 8000)
+  await app.listen(process.env.PORT || 8000, '0.0.0.0')
 }
 
 bootstrap()
